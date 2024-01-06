1c25eba014f9787882d2826cbfc61bd860f0f5c4
xabisavee <151728644+veexabisa@users.noreply.github.com>
2024-01-06 14:47:28 -0800

    Created an app.js file and added a basic search engine with no api

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
