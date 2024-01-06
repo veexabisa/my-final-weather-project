5a897c213166832ca015b4afd1ed9df550df05ca
xabisavee <151728644+veexabisa@users.noreply.github.com>
2024-01-06 15:41:30 -0800

    Added the search functionality using the API

--- src/app.js
+++ src/app.js
@@ -3,7 +3,26 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function searchCity(city) {
+  let apiKey = "144016bb2ce3odff4b8e4583ca9a5c1t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
+
+searchCity("Port Elizabeth");
