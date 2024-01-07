20edeafe272b3ec40d6d9075ce987c4dfa8eb1af
xabisavee <151728644+veexabisa@users.noreply.github.com>
2024-01-06 17:07:09 -0800

    Added more weather data

--- src/app.js
+++ src/app.js
@@ -20,9 +20,38 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = response.data.temperature.current;
   let cityElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSppedElement = document.querySelector("#wind-speed");
+  let dateTimeElement = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  dateTimeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSppedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes} `;
 }
 
 searchCity("Port Elizabeth");
